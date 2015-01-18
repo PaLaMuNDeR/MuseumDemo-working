5702710ca62d0b536860cfac36e87cf328f9a4bd
martianev@gmail.com <691a8a35>
2015-01-18 21:13:34 +0200

    Adding material button

--- app/src/main/java/com/dima/polimi/museumdemo/MainActivity.java
+++ app/src/main/java/com/dima/polimi/museumdemo/MainActivity.java
@@ -2,6 +2,7 @@ package com.dima.polimi.museumdemo;
 
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -12,7 +13,13 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
+             Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+            if (toolbar != null) {
+                setSupportActionBar(toolbar);
+            }
+        }
+
+
 
 
     @Override
